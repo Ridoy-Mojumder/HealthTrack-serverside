ed2f9b87c6521654b4a9502c1465a1f5cd6b1b38
Ridoy-Mojumder <hridoymoju463@gmail.com>
2024-06-22 19:08:44 +0600

    modify server

--- index.js
+++ index.js
@@ -91,17 +91,7 @@ async function run() {
       })
     }
 
-    // use verify admin after verifyToken
-    const verifyAdmin = async (req, res, next) => {
-      const email = req.decoded.email;
-      const query = { email: email };
-      const user = await userCollection.findOne(query);
-      const isAdmin = user?.role === 'admin';
-      if (!isAdmin) {
-        return res.status(403).send({ message: 'forbidden access' });
-      }
-      next();
-    }
+    
 
     //clearing Token
     app.post("/logout", async (req, res) => {
